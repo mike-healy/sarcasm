3a37f8822f622eb5bf09647dd2fb6b1d403b23ea
Mike Healy <design@mikehealy.com.au>
2021-05-22 10:43:15 +1000

    I missed these files

---
+++ components/Engine.vue
@@ -0,0 +1,70 @@
+<template>
+  <form>
+      <div>
+          <label for="original">Your Text</label>
+          <input v-model="input" type="text" id="original">
+      </div>
+      <div style="margin: 2rem 0;">
+          <div class="output">{{ sarcasticize() }}</div>
+      </div>
+  </form>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            input: 'Everyone is unique'
+        }
+    },
+
+    methods: {
+        sarcasticize() {
+            let t = this.input.split('')
+            let len = t.length
+            let out = ''
+
+            const u = c => c.toUpperCase()
+            const l = c => c.toLowerCase()
+
+            for (let i=0; i<len; i++) {
+                let fn = (i%2 !== 0) ? u : l
+                out += fn(this.input[i])
+            }
+
+            return out
+        }
+    }
+}
+</script>
+
+<style>
+form {
+    padding: 2rem;
+    margin: 0 12px;
+    background: linear-gradient(30deg, #f09703, #f1e69d);
+    color: #111;
+    text-align: left;
+    border-radius: 8px;
+    box-shadow: -4px 4px 8px 8px rgba(0, 0, 0, 0.17);
+}
+
+label {
+    display: block;
+}
+input[type=text] {
+    display: block;
+    padding: 4px;
+    background: #efefef;
+    border-radius: 4px;
+    color: #333;
+}
+
+div.output {
+    background: #333;
+    color: #fff;
+    border: 1px solid #787;
+    padding: 16px;
+    margin-top: 32px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">
+        Sarcastic Text
+      </h1>
+      <Engine />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+@apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+body {
+  background: #2b2044;
+  color: #fff;
+}
+
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  margin: 1em 0;
+  font-weight: 300;
+  font-size: 32px;
+  color: #ffc107;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
